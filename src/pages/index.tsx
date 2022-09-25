b9371160fa6b0099e5579cd116758275b2096ff4
Rodolfo P. Grião <rodolfopgriao@gmail.com>
2022-09-25 17:07:13 -0300

    Componentes Layout e Título

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div className={``}>
-      
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+        <span>Coteúdo</span>
+      </Layout>
     </div>
   )
 }
